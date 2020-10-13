af814dc5bb76d3c87d90d9dcb99dc44d0621f7c2
nNDVG <grishinkrd@yandex.ru>
2020-10-13 14:17:26 +0300

    fixed permissions

--- api/permissions.py
+++ api/permissions.py
@@ -7,5 +7,5 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
     """
 
     def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS or obj.author == request.user:
-            return request
+        return (request.method in permissions.SAFE_METHODS or
+                obj.author == request.user)
